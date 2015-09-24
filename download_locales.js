3c0e2b952627d7c70cf7fe3c85ba739ef4985751
Gustaf Nilsson Kotte <gustaf.nk@jayway.com>
2015-09-24 16:20:43 +0200

    Fix issues in locale downloader
    Project name was wrong
    Use dashes instead of underscores
    Add simple console logging
    Add HTTP error logging

--- download_locales.js
+++ download_locales.js
@@ -1,15 +1,22 @@
 var http = require('http');
+var path = require('path');
 var fs = require('fs');
 
-var projectName = 'nordnet-react-app';
-var locales = ['en_GB', 'sv_SE', 'da_DK', 'no_NB', 'fi_FI'];
+var projectName = 'nordnet-react-app-light';
+var locales = ['en-GB', 'sv-SE', 'da-DK', 'no-NB', 'fi-FI'];
 
 locales.forEach(function(locale){
 
-  var file = fs.createWriteStream(locale + '.json');
+  var fileName = path.join('src', 'l10n', 'phrase.' + locale + '.json');
+  var file = fs.createWriteStream(fileName);
   var url = 'http://nodejs.dev.nordnet.se:4000/api/translations/' + projectName + '/' + locale;
 
+  console.log('Save translation from', url, 'to', fileName);
   var request = http.get(url, function(response) {
+    if (response.statusCode !== 200) {
+      console.log("HTTP ERROR:", response.statusCode)
+    }
+
     response.pipe(file);
   });
 });
